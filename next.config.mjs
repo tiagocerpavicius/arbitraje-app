ee8a2b740188cb95e062a4dcdbe1f8350d0d2677
tiagocerpavicius <tiagocerpa@hotmail.com>
2026-05-12 18:59:25 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@std/testing/mock': false,
+      '@std/testing/bdd': false,
+      '@gadicc/fetch-mock-cache/runtimes/deno.ts': false,
+      '@gadicc/fetch-mock-cache/stores/fs.ts': false,
+    };
+    return config;
+  },
+};
+
 export default nextConfig;
